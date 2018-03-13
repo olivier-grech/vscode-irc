ff2112e7342519a85f3ecd327e214736931edd93
Olivier Grech <olivier.grech@gmail.com>
2018-03-13 17:15:52 +0100

    Use user input instead of configuration settings to open IRC instance

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import { workspace, languages, window, commands, ExtensionContext, Disposable } from 'vscode';
+import { workspace, languages, window, commands, ExtensionContext, Disposable, InputBoxOptions } from 'vscode';
 import ContentProvider, { generateUri } from './provider';
 import IrcInstance from './ircInstance';
 
@@ -10,20 +10,19 @@ export function activate(context: ExtensionContext) {
 	const providerRegistrations = Disposable.from(
 		workspace.registerTextDocumentContentProvider(ContentProvider.scheme, provider)
 	);
-	
+
 	// Register command that crafts an uri
 	// Open the dynamic document, and shows it in the next editor
 	const commandRegistration = commands.registerTextEditorCommand('extension.openIrc', editor => {
 		let ircConfiguration = workspace.getConfiguration('irc');
-		
+
 		var server = ircConfiguration.get('server') as string;
 		var port = ircConfiguration.get('port') as number;
 		var channel = ircConfiguration.get('channel') as string;
 		var nick = ircConfiguration.get('nick') as string;
 		var ircInstance = new IrcInstance(server, port, channel, nick);
-		
-		const uri = generateUri(ircInstance);
-		return workspace.openTextDocument(uri).then(doc => window.showTextDocument(doc, editor.viewColumn + 1));
+
+		askForServer(ircInstance, editor);
 	});
 
 	context.subscriptions.push(
@@ -31,4 +30,57 @@ export function activate(context: ExtensionContext) {
 		commandRegistration,
 		providerRegistrations
 	);
+}
+
+function askForServer(ircInstance: IrcInstance, editor) {
+	let options: InputBoxOptions = {
+		prompt: "Server: ",
+		placeHolder: "server"
+	}
+
+	window.showInputBox(options).then(value => {
+		ircInstance._server = value;
+		askForPort(ircInstance, editor);
+	});
+}
+
+function askForPort(ircInstance: IrcInstance, editor) {
+	let options: InputBoxOptions = {
+		prompt: "Port: ",
+		placeHolder: "port"
+	}
+
+	window.showInputBox(options).then(value => {
+		ircInstance._port = +value; // Cast value as a number
+		askForChannel(ircInstance, editor);
+	});
+}
+
+function askForChannel(ircInstance: IrcInstance, editor) {
+	let options: InputBoxOptions = {
+		prompt: "Channel: ",
+		placeHolder: "channel (without the #)"
+	}
+
+	window.showInputBox(options).then(value => {
+		ircInstance._channel = value;
+		askForNick(ircInstance, editor);
+	});
+}
+
+function askForNick(ircInstance: IrcInstance, editor) {
+	let options: InputBoxOptions = {
+		prompt: "Nick: ",
+		placeHolder: "nick"
+	}
+
+	window.showInputBox(options).then(value => {
+		ircInstance._nick = value;
+		openIrcDocument(ircInstance, editor);
+	});
+}
+
+function openIrcDocument(ircInstance: IrcInstance, editor) {
+	const uri = generateUri(ircInstance);
+	return workspace.openTextDocument(uri).then(doc => window.showTextDocument(doc, editor.viewColumn + 1));
 }
